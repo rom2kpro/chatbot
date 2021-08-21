607807a5345efcc439d3cdcaceda690f3af03559
rom2kpro <44729642+rom2kpro@users.noreply.github.com>
2021-08-21 13:28:23 +0700

    second commit

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ function sendMessage(senderId, message) {
 }
 
 app.set('port', process.env.PORT || 5000);
-app.set('ip', process.env.IP || "localhost");
+app.set('ip', process.env.IP || "0.0.0.0");
 
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
